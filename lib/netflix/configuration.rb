f7eb60413d7a057c492b84887b1003efed480357
Rob Ares <rob.ares@gmail.com>
2009-01-26 20:21:47 -0500

    changing oauth client setup so 0.3.0 works

--- lib/netflix/configuration.rb
+++ lib/netflix/configuration.rb
@@ -18,7 +18,7 @@ module Netflix
         :request_token_url => "http://api.netflix.com/oauth/request_token",
         :access_token_url  => "https://api-user.netflix.com/oauth/login",
         :authorize_url     => "http://api.netflix.com/oauth/access_token",
-        :scheme            => :body,
+        :scheme            => :query_string,
         :http_method       => :post,
         :signature_method  => "HMAC-SHA1",
         :site              => "http://api.netflix.com"
