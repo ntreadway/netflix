33eb9711f4ba875ea2c3247b696ce2e49857683b
Rob Ares <rob.ares@gmail.com>
2009-02-14 22:43:20 -0500

    adding configuration test

---
+++ test/client_configuration_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + "/test_helper")
+
+class CLientConfigurationTest < Test::Unit::TestCase
+
+  context "The Configuration class" do
+    
+    before do
+      Netflix::Configuration.application_name = "failweb"
+      Netflix::Configuration.consumer_token = "token"
+      Netflix::Configuration.consumer_secret = "secret"  
+    end
+    
+    it "should allow the client to get the :application name" do
+      Netflix::Configuration.application_name.should == "failweb"
+    end
+    
+    it "should allow the client to get the :consumer_token" do
+      Netflix::Configuration.consumer_token.should == "token"
+    end
+    
+    it "should allow the client to get the :consumer_secret" do
+      Netflix::Configuration.consumer_secret.should == "secret"
+    end
+    
+    it "should return the api version that it is using" do
+      Netflix::Configuration.api_version.should == "1.0"
+    end
+
+    it "should return a new OAuth::Consumer object everytime #build_consumer is called" do
+      Netflix::Configuration.build_consumer.should_not === Netflix::Configuration.build_consumer
+    end
+    
+  end
+
+end
